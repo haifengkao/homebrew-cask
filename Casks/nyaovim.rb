c7c84a1f6e511620c379b67f425e347f5b9e1db2
Hai Feng Kao <haifeng@cocoaspice.in>
2016-08-06 00:43:45 +0800

    update license and sha

--- Casks/nyaovim.rb
+++ Casks/nyaovim.rb
@@ -1,12 +1,12 @@
 cask 'nyaovim' do
   version '0.0.1'
-  sha256 '85653caf0c39eceb326e5c8ef11a8df522a05c4f2bfb3263cdcbf1bc938148c4'
+  sha256 'ffcc31dfda9b9b5ad5bbc7cc5472cfbf1c854d8dd898e22cbcf0e3bd52803540'
 
   url "https://github.com/haifengkao/NyaoVim/releases/download/0.0.1/nyaovim.zip"
 
   name 'nyaovim'
   homepage 'https://github.com/haifeng/nyaovim'
-  license :cca
+  license :cc
 
   app 'nyaovim.app'
 
